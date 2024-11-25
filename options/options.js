3df268fb10168509cc833041d0af949d8e5959c7
Crowdbotics <team@crowdbotics.com>
2024-11-25 11:36:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "notetaker-ai-50131",
+  url: "https://notetaker-ai-50131.botics.co",
+  api: "https://notetaker-ai-50131.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
